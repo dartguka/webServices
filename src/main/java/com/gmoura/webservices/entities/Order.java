6bcda238e323bc8096d04d4be444009a2d2ce9ab
dartguka <gustavo.dart@hotmail.com>
2026-05-26 00:15:05 -0300

    enum, OrderStatus

--- src/main/java/com/gmoura/webservices/entities/Order.java
+++ src/main/java/com/gmoura/webservices/entities/Order.java
@@ -1,6 +1,7 @@
 package com.gmoura.webservices.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gmoura.webservices.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -9,7 +10,7 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "tb_order")
-public class Order  implements Serializable {
+public class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -20,6 +21,9 @@ public class Order  implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -28,10 +32,11 @@ public class Order  implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,6 +54,16 @@ public class Order  implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return  OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
