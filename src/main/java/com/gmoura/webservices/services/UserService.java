8a332fb794b84e3693ae2cbb3c8ecd82ded7bb5c
dartguka <gustavo.dart@hotmail.com>
2026-06-21 17:02:05 -0300

    Exception handling - Update

--- src/main/java/com/gmoura/webservices/services/UserService.java
+++ src/main/java/com/gmoura/webservices/services/UserService.java
@@ -4,6 +4,7 @@ import com.gmoura.webservices.entities.User;
 import com.gmoura.webservices.repositories.UserRepository;
 import com.gmoura.webservices.services.exceptions.DatabaseException;
 import com.gmoura.webservices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
 
     }
 
