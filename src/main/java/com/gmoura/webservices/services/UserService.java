ebdb89c8a48a8271451fcde6b9ae4488cbe7f048
dartguka <gustavo.dart@hotmail.com>
2026-06-21 15:51:33 -0300

    Exception handling - findById

--- src/main/java/com/gmoura/webservices/services/UserService.java
+++ src/main/java/com/gmoura/webservices/services/UserService.java
@@ -2,6 +2,7 @@ package com.gmoura.webservices.services;
 
 import com.gmoura.webservices.entities.User;
 import com.gmoura.webservices.repositories.UserRepository;
+import com.gmoura.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
